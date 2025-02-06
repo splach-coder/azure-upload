3f8bc7025983b24916c5924b6626faaf214cd004
Anas <76971107+splach-coder@users.noreply.github.com>
2025-02-06 17:39:50 +0100

    creates new route for ncts formats and clean the data

---
+++ NCTS_FORMATER/__init__.py
@@ -0,0 +1,221 @@
+import azure.functions as func
+import logging
+import json
+
+from NCTS_FORMATER.functions.funcitons import combine_jsons_many_to_one_relation, combine_jsons_one_to_many_relation, compare_numbers_with_tolerance, remove_fields_from_json
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Processing file upload request.')
+
+    # Attempt to parse JSON body
+    try:
+        req_body = req.get_json()
+        file_data = req_body.get('file_data')
+        sql_data = req_body.get('sql_data').get('ResultSets').get('Table1')
+        email = {
+            "sendEmail" : False,
+            "emailBody" : ""
+        }
+
+        result = {}
+
+        #process the data from the sql if plda files is there
+        if len(sql_data)  > 0:
+            sql_data = sql_data[0]
+            items = sql_data.get("ITEMS")
+            items = json.loads(items)
+            sql_data["ITEMS"] = items
+        else:
+            email = {
+                "sendEmail" : True,
+                "emailBody" : f"""
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <p>Dear,</p>
+                    <p>Please check, we can't find this record on the PLDA database:</p>
+                    <p><strong>CONTAINER:</strong> {file_data.get("containers")}</p>
+                    <p>Best,</p>
+                    <p>MESSI10 ⚽</p>
+                </body>
+                </html>
+                """
+            }
+            return func.HttpResponse(
+                json.dumps(email),
+                mimetype="application/json",
+                status_code=400
+            )
+
+        #remove some fields from items in file_Data
+        items = file_data.get("items", "")
+        cleaned_data = []
+        for item in items:
+            fields_to_remove = ["packages", "description", "gross weight"]
+            cleaned_data.append(remove_fields_from_json(item, fields_to_remove))
+
+        file_data["items"] = cleaned_data
+
+        merged_items = []
+        pdf_items = file_data.get("items", "") 
+        plda_items = sql_data.get("ITEMS" , "")
+
+        #begin the merging engine
+        #ONE TO ONE RELATION
+        if len(pdf_items) == 1 and len(plda_items) == 1:
+            merged_items = [{**pdf_items[0], **plda_items[0]}]
+
+            PDF_GROSS = merged_items[0].get("Gross Weight")
+            PDF_PKGS = merged_items[0].get("Packages")
+            PLDA_GROSS = merged_items[0].get("GROSSMASS")
+            PLDA_PKGS = merged_items[0].get("PACKAGES")
+
+            if PLDA_PKGS != PDF_PKGS:
+                email = {
+                "sendEmail" : True,
+                "emailBody" : f"""
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <p>Dear,</p>
+                    <p>Please check ID {sql_data.get("DECLARATION_ID") }</p>
+                    <p><strong>PACKAGE IS DIFFERENT</strong></p>
+                    <p>Best,</p>
+                    <p>MESSI10 ⚽</p>
+                </body>
+                </html>
+                """
+                }
+            elif not compare_numbers_with_tolerance(PDF_GROSS, PLDA_GROSS):
+                email = {
+                "sendEmail" : True,
+                "emailBody" : f"""
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <p>Dear,</p>
+                    <p>Please check ID {sql_data.get("DECLARATION_ID") }</p>
+                    <p><strong>GROSS HAS A DIFFERNECE MORE THAN 5 KG</strong></p>
+                    <p>Best,</p>
+                    <p>MESSI10 ⚽</p>
+                </body>
+                </html>
+                """
+                }
+
+        #ONE TO MANY RELATION
+        elif len(pdf_items) == 1 and len(plda_items) >= 1:
+            merged_items = combine_jsons_one_to_many_relation(pdf_items[0], plda_items)
+
+            PDF_GROSS = pdf_items.get("Gross Weight")
+            PDF_PKGS = pdf_items.get("Packages")
+            PLDA_GROSS = sql_data.get("CONTROL_GROSSMASS")
+            PLDA_PKGS = sql_data.get("CONTROL_PACKAGES")
+
+            if PLDA_PKGS != PDF_PKGS:
+                email = {
+                "sendEmail" : True,
+                "emailBody" : f"""
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <p>Dear,</p>
+                    <p>Please check ID {sql_data.get("DECLARATION_ID") }</p>
+                    <p><strong>PACKAGE IS DIFFERENT</strong></p>
+                    <p>Best,</p>
+                    <p>MESSI10 ⚽</p>
+                </body>
+                </html>
+                """
+                }
+            elif not compare_numbers_with_tolerance(PDF_GROSS, PLDA_GROSS):
+                email = {
+                "sendEmail" : True,
+                "emailBody" : f"""
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <p>Dear,</p>
+                    <p>Please check ID {sql_data.get("DECLARATION_ID") }</p>
+                    <p><strong>GROSS HAS A DIFFERNECE MORE THAN 5 KG</strong></p>
+                    <p>Best,</p>
+                    <p>MESSI10 ⚽</p>
+                </body>
+                </html>
+                """
+                }
+
+        #MANY TO ONE RELATION
+        elif len(pdf_items) >= 1 and len(plda_items) == 1:
+            merged_items = combine_jsons_many_to_one_relation(pdf_items, plda_items[0])
+
+            PDF_GROSS = pdf_items.get("Gross Weight")
+            PDF_PKGS = pdf_items.get("Packages")
+            PLDA_GROSS = sql_data.get("CONTROL_GROSSMASS")
+            PLDA_PKGS = sql_data.get("CONTROL_PACKAGES")
+
+            if PLDA_PKGS != PDF_PKGS:
+                email = {
+                "sendEmail" : True,
+                "emailBody" : f"""
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <p>Dear,</p>
+                    <p>Please check ID {sql_data.get("DECLARATION_ID") }</p>
+                    <p><strong>PACKAGE IS DIFFERENT</strong></p>
+                    <p>Best,</p>
+                    <p>MESSI10 ⚽</p>
+                </body>
+                </html>
+                """
+                }
+            elif not compare_numbers_with_tolerance(PDF_GROSS, PLDA_GROSS):
+                email = {
+                "sendEmail" : True,
+                "emailBody" : f"""
+                <!DOCTYPE html>
+                <html>
+                <body>
+                    <p>Dear,</p>
+                    <p>Please check ID {sql_data.get("DECLARATION_ID") }</p>
+                    <p><strong>GROSS HAS A DIFFERNECE MORE THAN 5 KG</strong></p>
+                    <p>Best,</p>
+                    <p>MESSI10 ⚽</p>
+                </body>
+                </html>
+                """
+                }
+
+        #more cleaning
+        cleaned_data2 = []
+        for item in merged_items:
+            fields_to_remove = ["Packages", "Net Weight", "Gross Weight"]
+            cleaned_data2.append(remove_fields_from_json(item, fields_to_remove))
+
+        file_data["items"] = cleaned_data2
+        file_data["CONTROL_GROSSMASS"] = sql_data.get("CONTROL_GROSSMASS")
+        file_data["CONTROL_NETMASS"] = sql_data.get("CONTROL_NETMASS")
+        file_data["CONTROL_PACKAGES"] = sql_data.get("CONTROL_PACKAGES")
+        file_data["Email"] = email
+
+        try:
+            return func.HttpResponse(
+                json.dumps(file_data),
+                mimetype="application/json",
+                status_code=200
+            )
+
+        except Exception as e:
+            logging.error(f"Error: {e}")
+            return func.HttpResponse(
+                f"Error processing request: {e}", status_code=500
+            )   
+
+    except ValueError:
+        logging.error("Invalid JSON in request body.")
+        return func.HttpResponse(
+            body=json.dumps({"error": "Invalid JSON"}),
+            status_code=400,
+            mimetype="application/json"
+        )
---
+++ NCTS_FORMATER/functions/funcitons.py
@@ -0,0 +1,81 @@
+import json
+
+def combine_jsons_one_to_many_relation(json_one, json_two):
+    """
+    Combines a single JSON object (json_one) with a list of JSON objects (json_two).
+
+    :param json_one: dict, the first JSON with one item.
+    :param json_two: list of dicts, the second JSON with two or more items.
+    :return: list of combined JSON objects.
+    """
+    # Extract relevant fields from the first JSON
+    item = json_one["item"]
+    arrival_notice1 = json_one["ArrivalNotice1"]
+    arrival_notice2 = json_one["ArrivalNotice2"]
+    container = json_one["container"]
+    description = json_one["Description"]
+
+    # Combine fields with each item in the second JSON
+    combined_result = []
+    for entry in json_two:
+        combined_entry = {
+            "item": item,
+            "ArrivalNotice1": arrival_notice1,
+            "ArrivalNotice2": arrival_notice2,
+            "container": container,
+            "Description": description,
+            **entry  # Add the entire entry from json_two
+        }
+        combined_result.append(combined_entry)
+
+    return combined_result
+
+def combine_jsons_many_to_one_relation(json_one, json_two):
+    """
+    Combines a list JSON objects (json_one) with a single of JSON object (json_two).
+
+    :param json_one: dict, the first JSON with two or more items..
+    :param json_two: list of dicts, the second JSON with one item.
+    :return: list of combined JSON objects.
+    """
+
+    # Combine fields with each item in the second JSON
+    combined_result = []
+    for entry in json_one:
+        combined_entry = {
+            **json_two,
+            **entry  # Add the entire entry from json_two
+        }
+        combined_result.append(combined_entry)
+
+    return combined_result
+
+def remove_fields_from_json(json_data, fields_to_remove):
+    """
+    Removes specified fields from a JSON object or list of JSON objects.
+
+    :param json_data: dict or list of dicts, JSON data from which fields should be removed
+    :param fields_to_remove: list of str, field names to remove
+    :return: dict or list of dicts, JSON data with specified fields removed
+    """
+    if isinstance(json_data, dict):
+        return {key: value for key, value in json_data.items() if key not in fields_to_remove}
+    elif isinstance(json_data, list):
+        return [remove_fields_from_json(item, fields_to_remove) for item in json_data]
+    else:
+        raise TypeError("Invalid JSON data type. Expected dict or list of dicts.")
+    
+def compare_numbers_with_tolerance(num1, num2, tolerance=5):
+    """
+    Compares two numbers and checks if the absolute difference is within the given tolerance.
+
+    :param num1: float, the first number
+    :param num2: float, the second number
+    :param tolerance: float, the allowed absolute difference (default is 5)
+    :return: bool, True if the difference is within tolerance, False otherwise
+    """
+    # Calculate the absolute difference
+    absolute_difference = abs(num1 - num2)
+
+    # Check if the absolute difference is within the tolerance
+    return absolute_difference <= tolerance    
