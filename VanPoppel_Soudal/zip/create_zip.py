10bbaa7477413371df0c88df38ec758c8e431e2e
Anas <76971107+splach-coder@users.noreply.github.com>
2025-05-19 16:24:48 +0100

    refactor: Add checks for None values before writing files to zip

--- VanPoppel_Soudal/zip/create_zip.py
+++ VanPoppel_Soudal/zip/create_zip.py
@@ -5,8 +5,10 @@ def zip_excels(file1: BytesIO, file2: BytesIO, name1="file1.xlsx", name2="file2.
     zip_buffer = BytesIO()
 
     with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
-        zip_file.writestr(name1, file1.getvalue())
-        zip_file.writestr(name2, file2.getvalue())
+        if file1 is not None:
+            zip_file.writestr(name1, file1.getvalue())
+        if file2 is not None:       
+            zip_file.writestr(name2, file2.getvalue())
 
     zip_buffer.seek(0)
     return zip_buffer
