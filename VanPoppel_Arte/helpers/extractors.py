0be3e649b009187ac8b721cdb226b6db8dc67eab
Anas <anasbenabbou776@gmail.com>
2025-07-04 14:26:39 +0100

    fix: remove redundant keyword from invoice page extraction function

--- VanPoppel_Arte/helpers/extractors.py
+++ VanPoppel_Arte/helpers/extractors.py
@@ -221,7 +221,7 @@ def extract_customs_authorization_no(text):
         return match.group(1).strip()
     return None
 
-def find_page_in_invoice(doc, keywords=["Incoterms Version 2020", "Incoterms:"]):
+def find_page_in_invoice(doc, keywords=[ "Incoterms:"]):
     try:
         # Open the PDF file
         pdf_document = doc
